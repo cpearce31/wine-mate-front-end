9bdc98cb31512de1801a9526d90c9abc6ee837d7
constaac <aconstantine719@gmail.com>
2017-07-18 14:59:19 -0400

    Implements delete Feature
    Adds deleteEntry action into datatable component
    This action passes the entry up to the route that is rendering the DT
    
    The store is queried for the record, then the record is deleted, and
    finally the datatable row is deleted
    
    Adds deleteEntry="deleteEntry" to both calls of DT in WL/Inv.hbs
    
    closes #36 #10 #11

--- app/routes/inventories.js
+++ app/routes/inventories.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    deleteEntry: function (entry) {
+      this.get('store').findRecord('inventory', entry.id, { reload: true }).then(
+        function (entry) {
+          entry.destroyRecord();
+          return entry
+      }).then(
+        function (entry) {
+          $('#datatable').DataTable().row('#row-' + entry.id).remove().draw();
+          console.log('deleted!');
+      });
+    }
+  },
   model () {
     return this.get('store').findAll('inventory');
   },
