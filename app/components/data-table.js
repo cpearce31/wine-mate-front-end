6e4305e61362261765edcd9582638222a823a739
constaac <aconstantine719@gmail.com>
2017-07-18 12:08:04 -0400

    Changes table dib to id=datatable
    Tests OK

--- app/components/data-table.js
+++ app/components/data-table.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement(){
-    this.$('#example').DataTable({
+    this.$('#datatable').DataTable({
       select: 'single',
       stateSave: true,
       columnDefs: [
