f93d8f25cad15395fb57217bad440d28d1c424cf
constaac <aconstantine719@gmail.com>
2017-07-19 16:12:20 -0400

    Implements Add functionality for Inventory
    Follows same structure as previous commit
    
    Sets entry object values equal to form fields
    Creates/Saves record with entry
    If successful routes to inventories
    If failure fires error flashMessage
    
    closes #7

--- app/components/inv-editor.js
+++ app/components/inv-editor.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  router: Ember.inject.service("-routing"),
+  store: Ember.inject.service(),
+  entry: {
+    size: 'Standard',
+    winery: '',
+    name: '',
+    quantity: 1
+  },
+  actions: {
+    createEntry: function () {
+      this.get('store').createRecord('inventory', this.get('entry')).save().then(
+        () => {
+          this.get('router').transitionTo('inventories')
+        }
+      ).catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem adding your entry. Please try again.');
+      })
+    },
+    updateValue: function(value) {
+      this.set('entry.size', value);
+    },
+    updateEntry: function () {
+      console.log('updating entry')
+    }
+  }
 });
