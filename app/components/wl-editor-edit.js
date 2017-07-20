50c8f99d0cdc7fe4843b50c9fa3c1a46ebd6093e
constaac <aconstantine719@gmail.com>
2017-07-20 12:54:25 -0400

    Implements Edit Functionality for WishLists
    Uses computed property to build entry object
    
    Adds select option with disabled to prompt optional size change
    
    Finds record by id and then sets updated properties and saves
    If success, routes to wl route
    If failure, displays failure flashMessage
    
    Shortens ID in wl/inv route to just :id from :entry-id
      Dashes were a problem
    
    closes #8

--- app/components/wl-editor-edit.js
+++ app/components/wl-editor-edit.js
@@ -1,19 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  positionalParams: ['id', 'bottleSize'],
   router: Ember.inject.service("-routing"),
   store: Ember.inject.service(),
-  entry: {
-    size: 'Standard',
-    winery: '',
-    name: ''
-  },
+  winery: '',
+  name: '',
+  size: Ember.computed('bottleSize', function () {
+    return this.get('bottleSize');
+  }),
+  location: '',
+  vintage: null,
+  grape: '',
+  entry: Ember.computed('name', 'winery', 'size', 'location', 'vintage', 'grape', function () {
+    return {
+      id: this.get('id'),
+      name: this.get('name'),
+      winery: this.get('winery'),
+      size: this.get('size'),
+      location: this.get('location'),
+      vintage: this.get('vintage'),
+      grape: this.get('grape')
+    }
+  }),
   actions: {
     updateValue: function(value) {
-      this.set('entry.size', value);
+      this.set('size', value);
     },
     updateEntry: function () {
-      console.log('updating entry')
+      this.get('store').findRecord('wish_list', this.get('id')).then(
+        (record) => {
+          record.set('name', this.get('entry').name);
+          record.set('winery', this.get('entry').winery);
+          record.set('size', this.get('entry').size);
+          record.set('location', this.get('entry').location);
+          record.set('vintage', this.get('entry').vintage);
+          record.set('grape', this.get('entry').grape);
+          record.save();
+        }
+      ).then(() => {
+        this.get('router').transitionTo('wish_lists');
+      }).catch((error) => {
+        this.get('flashMessages')
+        .danger('There was a problem editing your entry. Please try again.');
+      })
     }
   }
 });
